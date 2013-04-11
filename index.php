6bcf26609bf7e05a67c26acc2fe89e1f7ec19508
Joshua Smith <joshua-smith@verizon.net>
2013-04-11 15:40:56 -0300

    Update and rename Web Site to index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<DOCTYPE html>
+<html>
+  <head>
+    <title>Messaging made Easy | Chatterbox</title>
+  </head>
+  <body>
+    <img src=https://secure.gravatar.com/avatar/e7e63543c9193db55fca9000dfa22400?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-org-420.png alt="logo" />
+    <h1>Welcome to the Chatterbox website!</h1>
+    <p>This is a web page for the chatterbox app.</p>
+    <p>Come here to download the app, and to read about the planed updates and to see the latest version.</p>
+  </body>
+</html>

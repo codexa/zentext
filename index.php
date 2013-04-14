b37c6087772e550b9b9d7452bdd41f78ccdba83e
Joshua Smith <joshua-smith@verizon.net>
2013-04-13 22:48:25 -0300

    little update.

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <DOCTYPE html>
 <html>
   <head>
-    <title>Chatterbox Messaging made Easy</title>
+    <title>Messaging Made Easy | Chatterbox</title>
   </head>
   <body>
-    <img src=https://secure.gravatar.com/avatar/e7e63543c9193db55fca9000dfa22400?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-org-420.png alt="logo" />
-    <h1>Welcome to the Chatterbox website!</h1>
-    <p>This is a web page for the chatterbox app.</p>
+    <img src="https://secure.gravatar.com/avatar/e7e63543c9193db55fca9000dfa22400?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-org-420.png" alt="logo" />
+    <h1>Welcome to Chatterbox!</h1>
+    <p>This is a web page for the Chatterbox app.</p>
     <p>Come here to download the app, and to read about the planed updates/bug fixes, and to see the latest version.</p>
   </body>
 </html>

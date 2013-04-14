a466ccaed80a2dfa197690ef2c1c5cd295c41426
Joshua Smith <joshua-smith@verizon.net>
2013-04-13 22:56:03 -0300

    Added style, other <head> items

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <DOCTYPE html>
 <html>
   <head>
+    <!-- Note: will replace this with a standard header file when I have time (using php include) -->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="description" content="Chatterbox messaging app." />
+    <meta name="keywords" content="Messaging, Texting, Email, Communications" />
+  
     <title>Messaging Made Easy | Chatterbox</title>
+    
+    <!-- Stylesheets -->    
+    <link rel="stylesheet" type="text/css" href="/style/main.css" />
+    
+    <!-- Favicons -->    
+    <link href="/style/assets/favicon.png" rel="icon" type="image/x-icon" />
+    <link href="/style/assets/favicon.png" rel="shortcut icon" type="image/x-icon" />
   </head>
   <body>
     <img src="https://secure.gravatar.com/avatar/e7e63543c9193db55fca9000dfa22400?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-org-420.png" alt="logo" />

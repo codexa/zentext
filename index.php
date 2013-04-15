4ef663e689efd0272561e440d832fb6e500c60f3
Dennis Talbot <worldallstars@hotmail.com>
2013-04-15 10:43:11 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
     <main class="page" theme="white">
       <img src="/style/assets/logo.png" alt="logo" height="200" width="200" />
       <h1>Welcome to Chatterbox!</h1>
-      <p>This is a web page for the Chatterbox app.</p>
-      <p>Come here to download the app, and to read about the planed updates/bug fixes, and to see the latest version.</p>
+      <p>This is a web page for the ZenTalk app.</p>
+      <p>Come here to use the app, and to read about the planed updates/bug fixes, and to see the latest version.</p>
     </main>
   </body>
 </html>

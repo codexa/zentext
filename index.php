8ea4d4158d44b778ec6b07ded7ad78965bd0a7c0
Joshua Smith <joshua-smith@verizon.net>
2013-04-15 09:20:39 -0300

    Add header

--- index.php
+++ index.php
@@ -1,24 +1,16 @@
 <DOCTYPE html>
 <html>
   <head>
-    <!-- Note: will replace this with a standard header file when I have time (using php include) -->
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <meta name="description" content="Chatterbox messaging app." />
-    <meta name="keywords" content="Messaging, Texting, Email, Communications" />
-  
-    <title>Messaging Made Easy | Chatterbox</title>
-    
-    <!-- Stylesheets -->    
-    <link rel="stylesheet" type="text/css" href="/style/main.css" />
-    
-    <!-- Favicons -->    
-    <link href="/style/assets/favicon.png" rel="icon" type="image/x-icon" />
-    <link href="/style/assets/favicon.png" rel="shortcut icon" type="image/x-icon" />
+    <?php include '/lib/head.php'; ?>    
+    <title class="add">Messaging Made Easy</title>
   </head>
   <body>
-    <img src="https://secure.gravatar.com/avatar/e7e63543c9193db55fca9000dfa22400?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-org-420.png" alt="logo" />
-    <h1>Welcome to Chatterbox!</h1>
-    <p>This is a web page for the Chatterbox app.</p>
-    <p>Come here to download the app, and to read about the planed updates/bug fixes, and to see the latest version.</p>
+    <?php include '/lib/header.php'; ?>  
+    <main class="page" theme="white">
+      <img src="/style/assets/logo.png" alt="logo" />
+      <h1>Welcome to Chatterbox!</h1>
+      <p>This is a web page for the Chatterbox app.</p>
+      <p>Come here to download the app, and to read about the planed updates/bug fixes, and to see the latest version.</p>
+    </main>
   </body>
 </html>

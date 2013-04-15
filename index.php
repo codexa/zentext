c3e48b4d7b953acb6bb704b6d81a736b6cdf3c74
Joshua Smith <joshua-smith@verizon.net>
2013-04-15 09:29:36 -0300

    a url fix

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <DOCTYPE html>
 <html>
   <head>
-    <?php include '/lib/head.php'; ?>    
+    <?php include 'lib/head.php'; ?>    
     <title class="add">Messaging Made Easy</title>
   </head>
   <body>
-    <?php include '/lib/header.php'; ?>  
+    <?php include 'lib/header.php'; ?>  
     <main class="page" theme="white">
-      <img src="/style/assets/logo.png" alt="logo" />
+      <img src="/style/assets/logo.png" alt="logo" height="200" width="200" />
       <h1>Welcome to Chatterbox!</h1>
       <p>This is a web page for the Chatterbox app.</p>
       <p>Come here to download the app, and to read about the planed updates/bug fixes, and to see the latest version.</p>

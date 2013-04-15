68256faa804000a7acbd7e128819513705c9e9a4
Joshua Smith <joshua-smith@verizon.net>
2013-04-15 09:01:21 -0300

    Added header

--- lib/header.php
+++ lib/header.php
@@ -1,1 +1,19 @@
-<!-- This File will contain shared header code -->
+<header theme="blue">
+  <div class="container">
+    <a href="/" class="logo"><img alt="Chatterbox" src="/style/assets/logo.png" /></a>
+    <nav class="top_menu" id="menu_1">
+      <img class="mobile_button mobile-object" alt="Menu" src="/style/assets/menu.png" onClick="menuToggle('1')" />
+      <ul class="items">
+        <li><a href="/" type="button" id="nav_">Home</a></li>
+        <!--<li><a href="/app" type="button" id="nav_app">App</a></li>-->
+        <li><a href="/about" type="button" id="nav_about">About</a></li>
+      </ul>
+    </nav>
+  </div>    
+</header>
+
+<script type="text/javascript">
+  function menuToggle(number) {
+    document.getElementById('menu_'+number).classList.toggle('shown');
+  }
+</script>

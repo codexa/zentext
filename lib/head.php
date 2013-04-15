74d24048d2c8f8b7d79e0222a44efd6a449ae1ee
Joshua Smith <joshua-smith@verizon.net>
2013-04-15 09:11:07 -0300

    Create head.php

---
+++ lib/head.php
@@ -0,0 +1,18 @@
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="description" content="Chatterbox messaging app." />
+    <meta name="keywords" content="Messaging, Texting, Email, Communications" />
+    
+    <!-- Stylesheets -->    
+    <link rel="stylesheet" type="text/css" href="/style/main.css" />
+    
+    <!-- Favicons -->    
+    <link href="/style/assets/favicon.png" rel="icon" type="image/x-icon" />
+    <link href="/style/assets/favicon.png" rel="shortcut icon" type="image/x-icon" />
+
+    <script type="text/javascript">
+      if (document.title == null | document.title == undefined | document.title == '') {
+        document.title = 'Micovision';
+      } else {
+        document.title = (document.title + ' | Micovision');
+      }
+    </script>

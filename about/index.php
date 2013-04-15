4942607b9fbfc877dcea8285bbc038194877b2b6
Joshua Smith <joshua-smith@verizon.net>
2013-04-15 16:33:15 -0400

    More meditations and an about page!

---
+++ about/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <?php include '../lib/head.php'; ?>    
+  <title>About Zentext</title>
+  <body>
+    <?php include '../lib/header.php'; ?>  
+    <main class="page" theme="white">
+      <img src="/style/assets/logo_light.png" alt="logo" height="150" />
+      <h1>About Zentext</h1>
+      <p />Zentext is an organization that started on <a href="http://codecademy.com/">Codecademy</a>.  We are currently developing an open-source messaging app that runs in most browsers without plugins.
+    </main>
+  </body>
+</html>

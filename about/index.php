ee1b9b23f3da7b1ea87d656bea6d39e32253e64d
Kickingdust <florencecef@gmail.com>
2013-04-19 07:35:57 -0500

    reword text before join link
    Changed "You can join us by clicking" to "You can join our codecademy group by clicking"

--- about/index.php
+++ about/index.php
@@ -7,7 +7,7 @@
     <main class="page" theme="white">
       <img src="/style/assets/logo_light.png" alt="logo" height="150" />
       <h1>About Zentext</h1>
-      <p />Zentext is an organization that started on <a href="http://codecademy.com/">Codecademy</a>.  We are currently developing an open-source messaging app that runs in most browsers without plugins.  You can join us by clicking <a href="www.codecademy.com/groups/chatterbox/invite/92c1a494af1f81f3c35e839723bfb14d">this link</a>.
+      <p />Zentext is an organization that started on <a href="http://codecademy.com/">Codecademy</a>.  We are currently developing an open-source messaging app that runs in most browsers without plugins.  You can join our codecademy group by clicking <a href="www.codecademy.com/groups/chatterbox/invite/92c1a494af1f81f3c35e839723bfb14d">this link</a>.
       <p />We appreciate your patience while we are meditating on more to say!
     </main>
   </body>

76f4710ff156a75454f82c23607af7c7b6451b60
Kickingdust <florencecef@gmail.com>
2013-04-22 11:46:21 -0500

    Test removal of home button
    title explains all.

--- lib/header.php
+++ lib/header.php
@@ -1,11 +1,11 @@
 <header class="theme-dark">
   <div class="container">
     <a href="/" class="logo"><img alt="Zentext" src="/style/assets/logo_white_little.png" /></a>
-    <nav class="top_menu" id="menu_1">
+   <!-- <nav class="top_menu" id="menu_1">
       <img class="mobile_button mobile-object" alt="Menu" src="/style/assets/menu.png" onClick="menuToggle('1')" />
       <ul class="items">
         <li><a href="/" type="button" id="nav_">Home</a></li>
-        <!--<li><a href="/app" type="button" id="nav_app">App</a></li>-->
+          <li><a href="/app" type="button" id="nav_app">App</a></li>-->
         <li><a href="/about" type="button" id="nav_about">About</a></li>
       </ul>
     </nav>

23a9ec87d5f20ca5d931e8689d2bee714c84c2dc
Joshua Smith <joshua-smith@verizon.net>
2013-04-22 14:01:02 -0300

    Added version function

--- scripts/site-min.js
+++ scripts/site-min.js
@@ -2,4 +2,14 @@
 
 function init() {
   // Function that is called on page load.
+  dispVersion();
+}
+
+function dispVersion() {
+  if (document.getElementById('current-version-name')) {
+    document.getElementById('current-version-name').textContent = 'Development';
+  }
+  if (document.getElementById('current-version-date')) {
+    document.getElementById('current-version-date').textContent = 'Not Released Yet!';
+  }
 }

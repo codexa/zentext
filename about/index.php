ff0c8ac5ac02115e64dbf6902d79486cb608e068
Jack Doiron <jack83307@gmail.com>
2013-04-23 16:13:57 -0400

    changed theme="white" to class theme-white in about page

--- about/index.php
+++ about/index.php
@@ -4,7 +4,7 @@
   <title>About Zentext</title>
   <body>
     <?php include '../lib/header.php'; ?>  
-    <main class="page" theme="white">
+    <main class="page theme-white">
       <img src="/style/assets/logo_light.png" alt="logo" height="150" />
       <h1>About Zentext</h1>
       <p />Zentext is an organization that started on <a href="http://codecademy.com/">Codecademy</a>.  We are currently developing an open-source messaging app that runs in most browsers without plugins.  You can join our codecademy group by clicking <a href="http://www.codecademy.com/groups/chatterbox/invite/92c1a494af1f81f3c35e839723bfb14d">this link</a>.

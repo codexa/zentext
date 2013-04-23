8b6702cdebea97b3363b359ba3035b1e2b510b16
Kickingdust <florencecef@gmail.com>
2013-04-23 07:31:56 -0500

    Removed "Home" button

--- lib/header.php
+++ lib/header.php
@@ -4,7 +4,6 @@
    <nav class="top_menu" id="menu_1">
       <img class="mobile_button mobile-object" alt="Menu" src="/style/assets/menu.png" onClick="menuToggle('1')" />
       <ul class="items">
-        <li><a href="/" type="button" id="nav_">Home</a></li>
          <!--<li><a href="/app" type="button" id="nav_app">App</a></li>-->
         <li><a href="/about" type="button" id="nav_about">About</a></li>
       </ul>

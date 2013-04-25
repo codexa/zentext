1cd742e440044b30bc457cb13f7e0272f8ddd191
Kickingdust <florencecef@gmail.com>
2013-04-25 11:54:20 -0500

    Update fonts (Except headers)
    Changed paragraph font to Calibri

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <main class="page theme-light">
       <img src="/style/assets/logo_light.png" alt="logo" height="150" />
       <h1>Welcome to Zentext!</h1>
-      <p />Welcome to the Zentext webpage!
-      <p />Come here to use the app, and to read about the planed updates/bug fixes, and to see the latest version!
+      <p style="font-family:Calibri;">Welcome to the Zentext webpage!
+      <p style="font-family:Calibri;">Come here to use the app, and to read about the planed updates/bug fixes, and to see the latest version!
       <div class="sidebar version-container">
         Latest version: <span id="current-version-name" class="version-name"></span>
         <br />Release date: <span id="current-version-date" class="version-date"></span>

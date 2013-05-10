a232fa65d698abb0cebea2ac8a5b2a53aef37192
Dennis Talbot <worldallstars@hotmail.com>
2013-05-10 12:20:50 -0500

    correct spelling

--- about/index.php
+++ about/index.php
@@ -8,7 +8,7 @@
       <img src="/style/assets/logo_light.png" alt="logo" height="150" />
       <h1>About Zentext</h1>
       <p>Zentext is an organization that started on <a href="http://codecademy.com/">Codecademy</a>.  We are currently developing an open-source messaging app that runs in most browsers without plugins.  You can join our codecademy group by clicking <a href="http://www.codecademy.com/groups/chatterbox/invite/92c1a494af1f81f3c35e839723bfb14d">this link.</a>.</p>
-      <p>We hope to have the service up soon, but we need more members to help us! join are group if you wish to help!
+      <p>We hope to have the service up soon, but we need more members to help us! join our group if you wish to help!
       <p>We appreciate your patience while we are meditating on more to say!</p>
     </main>
   </body>

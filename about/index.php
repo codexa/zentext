7bd9a89d8350e84a179f29e25a4ff4730fe4fa69
Dennis Talbot <worldallstars@hotmail.com>
2013-05-11 13:49:00 -0500

    Update index.php

--- about/index.php
+++ about/index.php
@@ -7,7 +7,7 @@
     <main class="page theme-white">
       <img src="/style/assets/logo_light.png" alt="logo" height="150" />
       <h1>About Zentext</h1>
-      <p>Zentext is an organization that started on <a href="http://codecademy.com/">Codecademy</a> by Joshua Smith.  We are currently developing an open-source messaging app that runs in most browsers without plugins.  You can join our codecademy group by clicking <a href="http://www.codecademy.com/groups/chatterbox/invite/92c1a494af1f81f3c35e839723bfb14d">this link.</a>.</p>
+      <p>Zentext is an organization that started on <a href="http://codecademy.com/">Codecademy</a> by Joshua Smith in early 2013.  We are currently developing an open-source messaging app that runs in most browsers without plugins.  You can join our codecademy group by clicking <a href="http://www.codecademy.com/groups/chatterbox/invite/92c1a494af1f81f3c35e839723bfb14d">this link.</a>.</p>
       <p>We hope to have the service up soon, but we need more members to help us! join our group if you wish to help!
       <p>We appreciate your patience while we are meditating on more to say!</p>
     </main>

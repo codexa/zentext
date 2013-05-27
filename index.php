e124c5b4b784eb10f79550ba34c4ce67311e5bf7
Dennis Talbot <worldallstars@hotmail.com>
2013-05-27 18:29:24 -0500

    add link to test site

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
       <img src="/style/assets/logo_light.png" alt="logo" height="150" />
       <h1>Welcome to Zentext!</h1>
       <p>Welcome to the Zentext webpage!</p>
-      <p>Come here to use the app, and to read about the planed updates/bug fixes, and to see the latest version!</p>
+      <p>Come here to < a href =  http://zentextapp.appspot.com/test.html>use the app,</a> and to read about the planed updates/bug fixes, and to see the latest version!</p>
       <div class="sidebar version-container">
         Latest version: <span id="current-version-name" class="version-name"></span>
         <br />Release date: <span id="current-version-date" class="version-date"></span>

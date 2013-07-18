7e00b17b6ae04b09a9c94e23f02569bc7af92660
Joshua Smith <joshua-smith@verizon.net>
2013-07-18 14:12:12 -0400

    Add portal to header

--- lib/header.php
+++ lib/header.php
@@ -4,9 +4,9 @@
    <nav class="top_menu" id="menu_1">
       <img class="mobile_button mobile-object" alt="Menu" src="/style/assets/menu.png" onClick="menuToggle('1')" />
       <ul class="items">
-         <!--<li><a href="/app" type="button" id="nav_app">App</a></li>-->
+        <li><a href="http://portal.testserver.bugs3.com/" type="button" id="nav_portal">Portal</a></li>
         <li><a href="/about" type="button" id="nav_about">About</a></li>
-      <li><a href="/Change%20Log" type="button" id="nav_change log">Change Log</a></li>
+        <li><a href="/Change%20Log" type="button" id="nav_change log">Change Log</a></li>
       </ul>
     </nav>
   </div>    

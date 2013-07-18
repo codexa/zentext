97c56cf0332b9062b3550b0217418d7a298848ad
Joshua Smith <joshua-smith@verizon.net>
2013-07-18 14:31:27 -0400

    Fix head indentation

--- lib/head.php
+++ lib/head.php
@@ -1,24 +1,24 @@
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <meta name="description" content="Zentext messaging app." />
-    <meta name="keywords" content="Zentext, Messaging, Texting, Email, Communications" />
-    
-    <title><?php
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="description" content="Zentext messaging app." />
+  <meta name="keywords" content="Zentext, Messaging, Texting, Email, Communications" />
+
+  <title><?php
     if (isset($title)) {
       print $title." | ";
     }
-    ?>Zentext</title>
-    
-    <!-- Stylesheets -->    
-    <link rel="stylesheet" type="text/css" href="/style/main.css" />
-    <link rel="stylesheet" type="text/css" href="/style/responsive.css" />
-    <link rel="stylesheet" type="text/css" href="/style/elements.css" />
-    
-    <!-- Favicons -->    
-    <link href="/style/assets/favicon.ico" rel="icon" type="image/x-icon" />
-    <link href="/style/assets/favicon.ico" rel="shortcut icon" type="image/x-icon" />
-    
-    <!-- Scripts -->
-    <script type="text/javascript" src="/scripts/site-min.js"></script>
-  </head>
-  
+  ?>Zentext</title>
+
+  <!-- Stylesheets -->    
+  <link rel="stylesheet" type="text/css" href="/style/main.css" />
+  <link rel="stylesheet" type="text/css" href="/style/responsive.css" />
+  <link rel="stylesheet" type="text/css" href="/style/elements.css" />
+
+  <!-- Favicons -->    
+  <link href="/style/assets/favicon.ico" rel="icon" type="image/x-icon" />
+  <link href="/style/assets/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+
+  <!-- Scripts -->
+  <script type="text/javascript" src="/scripts/site-min.js"></script>
+</head>
+

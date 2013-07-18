9a6887efaa91eec20bed752108ff1e87473c90e7
Joshua Smith <joshua-smith@verizon.net>
2013-07-18 09:40:18 -0400

    Link to portal

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$title = "Messageing Made Easy";
+$title = "Messaging Made Easy";
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,7 +10,7 @@ $title = "Messageing Made Easy";
       <img src="/style/assets/logo_light.png" alt="logo" height="150" />
       <h1>Welcome to Zentext!</h1>
       <p>Welcome to the Zentext webpage!</p>
-      <p>Come here to <a href =  http://zentextapp.appspot.com/test.html>use the app,</a> and to read about the planed updates/bug fixes, and to see the latest version!</p>
+      <p>Come here to <a href="http://portal.testserver.bugs3.com/">use the app,</a> and to read about the planed updates/bug fixes, and to see the latest version!</p>
       <div class="sidebar version-container">
         Latest version: <span id="current-version-name" class="version-name"></span>
         <br />Release date: <span id="current-version-date" class="version-date"></span>

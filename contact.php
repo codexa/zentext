97b873e9b9bf8cbbfedbf0c6458163d35b0e47c3
HRanDEV <hrpearl01@gmail.com>
2013-07-26 23:59:09 +0100

    Add Social media links to Contact US

--- contact.php
+++ contact.php
@@ -7,6 +7,10 @@
   <?php include 'lib/include/header.php'; ?>
   <main class="page theme-light">
 	<p>Contact us by email <a href="mailto:codexa.zentext@gmail.com">codexa.zentext@gmail.com</a></p>
+	<p>or Contact us via Social Media:</p>
+	<a href="https://plus.google.com/114218432677511297119">Google+</a>
+	<a href="https://www.facebook.com/zentext.mapps">Facebook</a>
+	<a href="https://twitter.com/Zentext_App">Twitter</a>
   </main>
   <?php include 'lib/include/footer.php'; ?>
 </body>

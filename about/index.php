1e2d848280870a3efe91339302190756308a1240
Dennis Talbot <worldallstars@hotmail.com>
2013-07-27 18:47:58 -0500

    Update index.php
    Correct spelling

--- about/index.php
+++ about/index.php
@@ -11,7 +11,7 @@ $title = "About Zentext";
       <img src="//cdn.codexa.org/zentext/assets/img/logo_light.png" alt="logo" height="150" />
       <h1>About Zentext Alpha</h1>
       <p>Zentext is a messaging app that is made with open web standards by <a href="//codexa.org">Codexa</a></p>
-      <p>We hope to have the service up soon, butlease note that this is an alpha release, this means that it is unstable. We do not advise you to use yet and therefore do not take any liability for any harm.</p>
+      <p>We hope to have the service up soon, but please note that this is an alpha release, this means that it is unstable. We do not advise you to use yet and therefore do not take any liability for any harm.</p>
       <?php include 'lib/include/footer.php'; ?>
     </main>
   </body>
